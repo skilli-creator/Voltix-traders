d1e18f71fe06c88ac2c0091fc8ae49e77e3c5df0
skilli-creator <kyalotonny6@gmail.com>
2026-06-30 23:18:40 +0300

    Fix OAuth scope: change from trading to read

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -973,7 +973,7 @@ const TopPanel = ({ isSidebarOpen, onSidebarToggle }) => {
         </SidebarToggle>
 
         <Brand>
-          <span className="icon">◆</span>
+          <span className="icon">🔷</span>
           <span className="brand-text">
             <span className="voltix">Voltix Traders.</span>
             <span className="deriv">deriv</span>
